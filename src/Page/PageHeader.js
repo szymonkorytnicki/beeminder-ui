e400ac1346ad0f7f543cbd67ba8505e9ef4c9741
Szymon Korytnicki <szymon.korytnicki@gmail.com>
2022-04-11 07:36:11 +0200

    loading indicator

--- src/Page/PageHeader.js
+++ src/Page/PageHeader.js
@@ -1,5 +1,16 @@
-import "./PageHeader.css";
+import './PageHeader.css'
+import { useIsFetching } from 'react-query'
 
-export function PageHeader({children}) {
-    return <div className='page-header'>{children}</div>;
+export function PageHeader({ children }) {
+    const isFetching = useIsFetching()
+    return (
+        <div className="page-header">
+            {children}
+            {isFetching ? (
+                <div class="page-header__fetching-indicator"></div>
+            ) : (
+                ''
+            )}
+        </div>
+    )
 }
