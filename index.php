cac569f874df91eb841929f45745d0e3e8aaa9a4
Szymon Korytnicki <szymon.korytnicki@gmail.com>
2022-06-24 16:03:24 +0200

    #30 add dotenv parsing

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<a href="https://www.beeminder.com/apps/authorize?client_id=XYZ&redirect_uri=XYZ&response_type=token">Authorise</a>
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+?>
+
+<a href="https://www.beeminder.com/apps/authorize?client_id=<?php echo $_ENV['CLIENT_ID']?>&redirect_uri=<?php echo $_ENV['CALLBACK_URL']?>&response_type=token">Authorise</a>
 <?php
 echo "hello php my old friend I have server for you again";
 ?>
